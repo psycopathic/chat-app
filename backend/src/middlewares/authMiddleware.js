1458e1324e70104799569586604c9f08df0821d6
Harsh Shrivastava <harshsrivastava0302@gmail.com>
2025-07-09 20:30:20 +0530

    this branch has the signup ui

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -6,7 +6,7 @@ import User from "../models/userModel.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
+    const token = req.cookies?.jwt;
     if (!token) {
       return res
         .status(401)
