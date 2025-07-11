09cf0fc394b6ddb0f841c4e12f73a91555fa27d7
Harsh Shrivastava <harshsrivastava0302@gmail.com>
2025-07-11 19:02:36 +0530

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 app.use('/api/auth',authRoutes);
-app.use('/api/message',messageRoutes);
+app.use('/api/messages',messageRoutes);
 
 
 
